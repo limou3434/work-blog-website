f05596dadb09259315b550cccdfaa9b4485355f6
limou3434 <898738804@qq.com>
2026-01-28 22:13:50 +0800

    成功引入 UI 组件

--- work-blog-website-frontend/docs/.vuepress/client.ts
+++ work-blog-website-frontend/docs/.vuepress/client.ts
@@ -10,13 +10,13 @@ import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
 // @ts-ignore
 import BulletinContent from './components/BulletinContent.vue'
 // @ts-ignore
-import AiChat from './components/AiChat.vue'
+import TestAntdUI from './components/TestAntdUI.vue'
 
 export default defineClientConfig({
     enhance({app}) {
         app.component('RepoCard', RepoCard); // 引入码仓卡片组件
         app.component('Swiper', Swiper) // 引入轮播图片组件
         app.component('BulletinContent', BulletinContent); // 引入全局公告组件
-        app.component('AiChat', AiChat) // 引入轮播图片组件
+        app.component('TestAntdUI', TestAntdUI) // 引入轮播图片组件
     },
 })
---
+++ work-blog-website-frontend/docs/.vuepress/components/TestAntdUI.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="test-antd-ui"></div>
+  <a-space wrap>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </a-space>
+</template>
+
+<script setup>
+import { Button as AButton } from 'ant-design-vue'
+</script>
